88aa545554128a427eeed0352bd4a89d69fe94ff
febriyan <setiyawantegar06@gmial.com>
2024-08-14 09:25:56 +0700

    [FEATURE] update fitur backend for upload file

--- app.js
+++ app.js
@@ -8,11 +8,19 @@ import productRouter from "./routes/produtcRouter.js";
 import orderRouter from "./routes/orderRouter.js";
 import helmet from "helmet";
 import ExpressMongoSanitize from "express-mongo-sanitize";
+import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
 
 // middleware
 app.use(express.json()); // Parsing JSON body
--- utils/uploadFileHandler.js
+++ utils/uploadFileHandler.js
@@ -1,29 +1,4 @@
 import multer from "multer";
-import path from "path";
 
-const FILE_TYPE = {
-  "image/png": "png",
-  "image/jpeg": "jpeg",
-  "image/jpg": "jpg",
-};
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    const isValidFormat = FILE_TYPE[file.mimetype];
-
-    let uploadError = new Error("Invalid format image ");
-
-    if (isValidFormat) {
-      uploadError = null;
-    }
-    cb(uploadError, "./public/upload");
-  },
-  filename: function (req, file, cb) {
-    const uniquieFile = `${file.filename}-${Date.now()}${path.extname(
-      file.originalname
-    )}`;
-
-    cb(null, uniquieFile);
-  },
-});
-
+const storage = multer.memoryStorage();
 export const upload = multer({ storage: storage });
